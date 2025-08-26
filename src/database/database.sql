8a7661536929af163208fb301c537e672cf2d93e
CaioSousavs <joao.sousa@sptech.school>
2025-08-25 21:11:00 -0300

    atualizando banco de dados

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,7 +1,6 @@
 create database PayMetrics;
 use PayMetrics;
 
-
 create table endereco (
 id_endereco int auto_increment,
 logradouro varchar(200) not null,
@@ -35,10 +34,10 @@ references endereco(id_endereco)
 );
 
 create table usuarios (
-id_usuario int not null auto_increment,
+id_usuario int auto_increment,
 nome varchar(200) not null,
-senha varchar(200) not null,
 email varchar(200) not null,
+senha varchar(200) not null,
 administrador boolean default false,
 fk_empresa int not null,
 primary key (id_usuario),
@@ -46,4 +45,33 @@ foreign key (fk_empresa) references empresa(id_empresa),
 unique (email)
 );
 
-update usuarios set senha = '${NovaSenha}' where id_usuario = '${ID_USUARIO}';
+insert into endereco (logradouro, numero, cidade, sigla_estado, sigla_pais, cartao_postal)
+values
+('7th Avenue', '410 Terry', 'Seattle', 'WA', 'US', '98109'),
+('Avenida Juscelino Kubitschek', '2041', 'São Paulo', 'SP', 'BR', '04543-011'),
+('Marcel-Breuer-Strasse', '12', 'Munique', 'BY', 'DE', '80807'),
+('Rue de l\Héronnière', '67', 'Clichy', 'IDF', 'FR', '92110'),
+('West Georgia Street', '402', 'Vancouver', 'BC', 'CA', 'V6B 5A1'),
+('Calle Ramírez de Prado', '5', 'Madrid', 'MD', 'ES', '28045');
+
+insert into empresa (razão_social, fk_endereco)
+values
+('Amazon.com Inc.', 1),
+('Amazon Brasil Ltda.', 2),
+('Amazon Deutschland GmbH', 3),
+('Amazon France SAS', 4),
+('Amazon Canada ULC', 5), 
+('Amazon Spain Services SL', 6); 
+
+insert into contato (nome_contato, telefone, email_contato, fk_empresa)
+values
+('Andy Jassy', '+1-206-555-0100', 'andy.jassy@amazon.com', 1),
+('Mariana Roth', '+55-11-5555-0101', 'mariana.roth@amazon.com.br', 2),
+('Ralf Kleber', '+49-89-5555-0102', 'ralf.kleber@amazon.de', 3),
+('Frédéric Duval', '+33-1-5555-0103', 'frederic.duval@amazon.fr', 4),
+('Jesse Dougherty', '+1-604-555-0104', 'jesse.dougherty@amazon.ca', 5),
+('Mariangela Marseglia', '+34-91-5555-0105', 'mariangela.marseglia@amazon.es', 6);
+
+select*from usuarios;
+
+-- update usuarios set senha = {NovaSenha} where id_usuario = {ID_USUARIO};
