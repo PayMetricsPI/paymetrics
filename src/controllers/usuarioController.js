38705e50047ff25e5547b0dc375b0e8898df5383
CaioSousavs <joao.sousa@sptech.school>
2025-08-26 21:06:58 -0300

    criando controllers da function listar usuarios.

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -121,8 +121,26 @@ function renovar(req, res) {
     }
 }
 
+    function listar(req, res ){
+        var IdEmpresa = req.params.IdEmpresa;
+
+        usuarioModel.listar(IdEmpresa).then(function(resultado){
+            if (resultado.length > 0){
+                res.status(200).json(resultado);
+            }else {
+                res.status(204).send("Nenhum resultado encontrado")
+            }
+        }).catch(function(erro){
+            console.log(erro);
+            console.log("Houve um erro ao buscar os usuários: ", erro.sqlMessage);
+            res.status(500).json(erro.sqlMessage);
+            
+        })
+    };
+
 module.exports = {
     autenticar,
     cadastrar,
-    renovar
+    renovar,
+    listar
 }
