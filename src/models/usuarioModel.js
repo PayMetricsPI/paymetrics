69a6fc180bda2814799eb02f77e5c1b91933b89a
CaioSousavs <joao.sousa@sptech.school>
2025-08-27 20:57:59 -0300

    Subindo model da function listar em usuarioModel.js

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -18,7 +18,7 @@ function cadastrar(nome, email, senha, administrador, fk_empresa) {
     return database.executar(instrucaoSql);
 }
 
-function renovar(id_usuario, senha, novaSenha) {
+function renovar(IdUsuario, senha, novaSenha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function renovar():",IdUsuario, novaSenha, senha);
     var instrucaoSql = `
        UPDATE usuarios SET senha = ${novaSenha} WHERE id_usuario = ${id_usuario};`;
@@ -26,8 +26,20 @@ function renovar(id_usuario, senha, novaSenha) {
     return database.executar(instrucaoSql);
 }
 
+function listar(IdEmpresa){
+    console.log("ACESSEI O USUARIO MODEL \n \n \t\t >> se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco \n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar():", IdEmpresa);
+    var instrucaoSql = ``;
+    console .log("Executando a instrução SQL \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+
+
 module.exports = {
     autenticar,
     cadastrar,
-    renovar
+    renovar,
+    listar,
+    deletar
 };
