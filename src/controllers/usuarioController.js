9da47638f065444fb53dfd9c85292a0673da9e27
Fernanda <fernanda.araujo@sptech.school>
2025-08-27 21:53:05 -0300

    Arrumando um ponto na página usuarioModel

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -24,7 +24,7 @@ function autenticar(req, res) {
                         email: resultadoAutenticar[0].email,
                         nome: resultadoAutenticar[0].nome,
                         senha: resultadoAutenticar[0].senha,
-                        administrador: resultadoAutenticar[0],administrador
+                        administrador: resultadoAutenticar[0].administrador
                         });
 
                     } else if (resultadoAutenticar.length == 0) {
