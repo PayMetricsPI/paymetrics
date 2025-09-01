a93e186e6795a56a7f981a285d836c8114c05339
EduardoNdL <eduardo.lima@sptech.school>
2025-09-01 11:48:26 -0300

    fix perfil

--- public/js/header.js
+++ public/js/header.js
@@ -8,12 +8,15 @@ if(sessionStorage.EMAIL_USUARIO) {
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 gap-4">
                     <li class="nav-item">
-                        <a class="nav-link" href="sobre.html">Sobre</a>
+                        <a class="nav-link" href="index.html#sobre">Sobre</a>
                     </li>
                     <li class="nav-item">
-                            <svg onclick="limparSessao()"  xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#000" d="M19.002 3h-14c-1.103 0-2 .897-2 2v4h2V5h14v14h-14v-4h-2v4c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V5c0-1.103-.898-2-2-2"/><path fill="#000" d="m11 16l5-4l-5-4v3.001H3v2h8z"/></svg>
+                            <a href="perfil.html"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 15 15"><rect width="15" height="15" fill="none"/><path fill="#000" fill-rule="evenodd" d="M.877 7.5a6.623 6.623 0 1 1 13.246 0a6.623 6.623 0 0 1-13.246 0M7.5 1.827a5.673 5.673 0 0 0-4.193 9.494A4.97 4.97 0 0 1 7.5 9.025a4.97 4.97 0 0 1 4.193 2.296A5.673 5.673 0 0 0 7.5 1.827m3.482 10.152A4.02 4.02 0 0 0 7.5 9.975a4.02 4.02 0 0 0-3.482 2.004A5.65 5.65 0 0 0 7.5 13.173c1.312 0 2.52-.446 3.482-1.194M5.15 6.505a2.35 2.35 0 1 1 4.7 0a2.35 2.35 0 0 1-4.7 0m2.35-1.4a1.4 1.4 0 1 0 0 2.8a1.4 1.4 0 0 0 0-2.8" clip-rule="evenodd"/></svg></a>
+                    </li>
+                    <li class="nav-item">
+                            <svg style="cursor: pointer;" onclick="limparSessao()"  xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#000" d="M19.002 3h-14c-1.103 0-2 .897-2 2v4h2V5h14v14h-14v-4h-2v4c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V5c0-1.103-.898-2-2-2"/><path fill="#000" d="m11 16l5-4l-5-4v3.001H3v2h8z"/></svg>
                     </li>
                 </ul>
             </div>
@@ -29,7 +32,7 @@ if(sessionStorage.EMAIL_USUARIO) {
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="sobre.html">Sobre</a>
+                        <a class="nav-link" href="index.html#sobre">Sobre</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="login.html">Login</a>
