5d9514d3382d994c8f579edf83bbb950bb9f58bb
EduardoNdL <eduardo.lima@sptech.school>
2025-09-01 10:46:48 -0300

    add perfil usuário

---
+++ public/js/perfil.js
@@ -0,0 +1,26 @@
+var usuario = sessionStorage.ID_USUARIO
+
+function carregarPerfil() {
+    fetch(`/usuarios/verificar/${usuario}`, { cache: 'no-store' })
+        .then(resposta => {
+            if (resposta.ok) {
+                if (resposta.status == 204) {
+                    var listaUsuarios = document.getElementsByClassName("mostrarUsuarios")[0];
+                    var mensagem = document.createElement("span");
+                    mensagem.innerHTML = "Nenhum resultado encontrado.";
+                    listaUsuarios.appendChild(mensagem);
+                    throw "Nenhum resultado encontrado!!";
+                }
+                resposta.json().then(resposta => {
+                    console.log(resposta)
+                    document.getElementById('nome').innerText = resposta.nome
+                    document.getElementById('email').innerText = resposta.email
+                    document.getElementById('empresa').innerText = resposta.empresa
+                });
+            } else {
+                throw "Houve um erro na API!";
+            }
+        }).catch(console.error);
+}
+
+window.onload = carregarPerfil;
