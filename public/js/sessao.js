0d583aceeddfdedceba8777005244a11bf62e193
EduardoNdL <eduardo.lima@sptech.school>
2025-09-05 15:04:08 -0300

    add validar adm

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -12,6 +12,18 @@ function validarSessao() {
     }
 }
 
+function validarAdmin() {
+    if(sessionStorage.ADMINISTRADOR == 1){
+        ul = document.getElementById("ul_links")
+        ul.innerHTML += `<li>
+                    <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="deletarUsuario.html">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#fff" d="M5 19h1.425L16.2 9.225L14.775 7.8L5 17.575zm-2 2v-4.25L16.2 3.575q.3-.275.663-.425t.762-.15t.775.15t.65.45L20.425 5q.3.275.438.65T21 6.4q0 .4-.137.763t-.438.662L7.25 21zM19 6.4L17.6 5zm-3.525 2.125l-.7-.725L16.2 9.225z"/></svg>
+                        <span class="user">Administrador</span>
+                    </a>
+                </li>`
+    }
+}
+
 function limparSessao() {
     sessionStorage.clear();
     window.location = "../login.html";
