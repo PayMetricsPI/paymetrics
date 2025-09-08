8e91ad744fc009e522fcbf3a8351e61276e121d9
Rafael Tokko <rafael.tokko@sptech.school>
2025-09-08 04:55:43 -0300

    configuração do header e sidebar

--- public/js/header.js
+++ public/js/header.js
@@ -1,28 +1,6 @@
-var header = document.getElementById("header")
-if(sessionStorage.EMAIL_USUARIO) {
-    console.log("sessionStorage.EMAIL_USUARIO:", sessionStorage.EMAIL_USUARIO);
-    header.innerHTML = ` <nav class="navbar navbar-expand-sm">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.html"><img src="./assets/icons/logo-paymetrics-sem-fundo 2.png""></img></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0 gap-4">
-                    <li class="nav-item">
-                        <a class="nav-link" href="embreve.html">Visão geral</a>
-                    </li>
-                    <li class="nav-item">
-                            <a href="perfil.html"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 15 15"><rect width="15" height="15" fill="none"/><path fill="#000" fill-rule="evenodd" d="M.877 7.5a6.623 6.623 0 1 1 13.246 0a6.623 6.623 0 0 1-13.246 0M7.5 1.827a5.673 5.673 0 0 0-4.193 9.494A4.97 4.97 0 0 1 7.5 9.025a4.97 4.97 0 0 1 4.193 2.296A5.673 5.673 0 0 0 7.5 1.827m3.482 10.152A4.02 4.02 0 0 0 7.5 9.975a4.02 4.02 0 0 0-3.482 2.004A5.65 5.65 0 0 0 7.5 13.173c1.312 0 2.52-.446 3.482-1.194M5.15 6.505a2.35 2.35 0 1 1 4.7 0a2.35 2.35 0 0 1-4.7 0m2.35-1.4a1.4 1.4 0 1 0 0 2.8a1.4 1.4 0 0 0 0-2.8" clip-rule="evenodd"/></svg></a>
-                    </li>
-                    <li class="nav-item">
-                            <svg style="cursor: pointer;" onclick="limparSessao()"  xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#000" d="M19.002 3h-14c-1.103 0-2 .897-2 2v4h2V5h14v14h-14v-4h-2v4c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V5c0-1.103-.898-2-2-2"/><path fill="#000" d="m11 16l5-4l-5-4v3.001H3v2h8z"/></svg>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>` 
-} else {
+var header = document.getElementById("header");
+var sidebar = document.getElementById("sidebar");
+if (header) {
     header.innerHTML = `<nav class="navbar navbar-expand-sm">
         <div class="container-fluid">
             <a class="navbar-brand" href="index.html"><img src="./assets/icons/logo-paymetrics-sem-fundo 2.png""></img></a>
@@ -41,4 +19,48 @@ if(sessionStorage.EMAIL_USUARIO) {
             </div>
         </div>
     </nav>` 
+} else if (sidebar) {
+    sidebar.innerHTML = ` 
+        <aside class="sidebar">
+        <nav class="navbar-color d-flex flex-column flex-shrink-0">
+        <div class="container-fluid d-flex w-100 align-items-center p-1 h-100">
+            <ul class="nav nav-pills nav-flush flex-column mb-auto text-center h-100" id="ul_links">
+                    <a class="d-flex p-3 link-dark text-decorartion-none" href="index.html">
+                        <img class="bi" width="40" height="32" src="assets/icons/PayMetrics-icon.png"></img>
+                    </a>
+                <li class="nav-item">
+                    <a class="nav-link py-3 d-flex mx-1 align-items-center" title data-bs-toggle="tooltip" data-bs-placemnet="right" arial-current="page" data-bs-original-title="Avisos" href="./embreve.html">
+                        <img src="assets/icons/alert-icon.png" class="bi mask1" aria-label="Avisos" width="32" height="32" alt="">
+                        <span class="alertas">Alertas</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                      <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./embreve.html">
+                        <img src="assets/icons/chart-icon.png" class="bi mask2" width="32" height="32" alt="">
+                        <span class="dash">Dashboards</span>
+                      </a>                    
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right"href="./embreve.html">
+                        <img src="assets/icons/settings-icon.png" class="bi mask3" width="32" height="32" alt="">
+                        <span class="config">Configurações</span>
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./perfil.html">
+                        <img role="img" class="bi mask" width="32" height="32" src="assets/icons/account-icon.png" alt="foto_perfil">
+                        <span class="user">Usuário</span>
+                    </a>
+                </li>
+                <li class="mt-auto">
+                    <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./index.html" onclick="limparSessao()">
+                        <img role="img" class="bi mask" width="32" height="32" src="assets/icons/iconesair.png" alt="foto_perfil">
+                        <span class="user">Sair</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  </aside>
+        `;
 }
