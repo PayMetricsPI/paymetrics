70d34a34b06f9bb8039720522629b9718d46d9f2
SamuelAntunes2006 <samuel.asantos@sptech.school>
2025-09-29 13:10:59 -0300

    feat: Adicionando criação do servidor

--- src/models/servidroModel.js
+++ src/models/servidroModel.js
@@ -1,5 +1,12 @@
 var database = require("../database/config")
 
+function criarServidor (nome, sistema_operacional,fk_empresa){
+    console.log("ACESSO AO SERIVIDOR MODEL")
+    var instrucaoSql = `insert into from servidor (nome,sistema_operacional,fk_empresa)Values (${nome}, ${sistema_operacional},${fk_empresa})`;
+    console.log("Executando a instrução SQL \n: " + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 function deletarServidor (id_servidor, fk_empresa){
     console.log("ACESSO AO SERIVIDOR MODEL")
     var instrucaoSql = `delete from servidor where id_servidor = ${id_servidor} and fk_empresa = ${fk_empresa}`;
@@ -8,5 +15,6 @@ function deletarServidor (id_servidor, fk_empresa){
 }
 
 module.exports={
+    criarServidor,
     deletarServidor
 };
