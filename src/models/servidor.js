2c192b3db32b112a8c9b6313038c565719416a54
SamuelAntunes2006 <samuel.asantos@sptech.school>
2025-09-29 12:27:28 -0300

    feat: Novas funcionalidades

---
+++ src/models/servidor.js
@@ -0,0 +1,22 @@
+var database = require("../database/config")
+
+function criarServidor(nome, sistema_operacional, fk_empresa) {
+    console.log("ACESSO AO SERVIDOR MODEL");
+    var instrucaoSql = `INSERT INTO servidor (nome, sistema_operacional, fk_empresa) VALUES ${nome}, ${sistema_operacional}, ${fk_empresa};`;
+    console.log("Executando a instrução SQL:\n" + instrucaoSql);
+
+    return database.executar(instrucaoSql);
+}
+
+function deletarServidor (id_servidor, fk_empresa){
+    console.log("ACESSO AO SERIVIDOR MODEL")
+    var instrucaoSql = `delete from servidor where id_servidor = ${id_servidor} and fk_empresa = ${fk_empresa}`;
+    console.log("Executando a instrução SQL \n: " + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports={
+    deletarServidor,
+    criarServidor
+};
