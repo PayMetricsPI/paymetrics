bb6a7e446e0b4fdadf2825ceba23c7c3487cbef3
Leo0192 <leonardo.tubero@sptech.school>
2025-10-24 10:50:56 -0300

    feat:iniciando a criação da model dos paramtros

---
+++ src/models/parametroModel.js
@@ -0,0 +1,44 @@
+var database = require("../database/config");
+
+function criarParametro(servidores) {
+    let queries = servidores.map(s => {
+        return `
+            insert into parametro (fk_componente,fk_empresa,fk_componente,alerta_critico, alerta_normal)
+            VALUES ('${s.fk_componente}','${s.fk_empresa}'),'${s.fk_componente}','${s.alerta_critico}', '${s.alerta_normal}';
+        `;
+    }).join("\n");
+
+    return database.executar(queries);
+}
+
+// function deletarParametro(id_servidor, fk_empresa) {
+
+//      if (id_servidor == null || fk_empresa == null) {
+//         console.error("Erro: id_servidor ou fk_empresa indefinidos!");
+//         return Promise.reject("id_servidor ou fk_empresa indefinidos");
+//     }
+    
+//     var instrucaoSql = `
+//         delete from servidor 
+//         where id_servidor = ${id_servidor} AND fk_empresa = ${fk_empresa};
+//     `;
+//     console.log("Executando a instrução SQL:\n" + instrucaoSql);
+//     return database.executar(instrucaoSql);
+// }
+
+function atualizarParametro(fk_componente,fk_empresa,fk_componente,alerta_critico, alerta_normal) {
+    var instrucaoSql = `
+        update paramtro
+        set fk_componente = '${fk_componente}', fk_empresa = '${fk_empresa}', fk_componente = '${fk_componente}',
+        alerta_critico = ${alerta_critico}, alerta_normal = ${alerta_normal}
+        where id_paramtro = ${id_paramtro};
+    `;
+    return database.executar(instrucaoSql);
+}
+
+
+module.exports = {
+    criarParametro,
+    // deletarParametro,
+    atualizarParametro
+};
