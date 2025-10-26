e55a9779227aae0a289a30bc6d4a94d0f5dba4eb
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-10-26 19:37:35 -0300

    fix: adaptando cadastro de usuarios (back-end) para atender a nova tabela cargo

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -17,9 +17,9 @@ function autenticar(req, res) {
                     console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`);
 
                     if (resultadoAutenticar.length == 1) {
-                    console.log(resultadoAutenticar);
+                        console.log(resultadoAutenticar);
 
-                    res.json(resultadoAutenticar[0]);
+                        res.json(resultadoAutenticar[0]);
 
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
@@ -39,63 +39,48 @@ function autenticar(req, res) {
 }
 
 function cadastrar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
-    var emailC = req.body.emailCServer;
     var senha = req.body.senhaServer;
-    var senhaC = req.body.senhaCServer;
     var fk_empresa = req.body.fk_empresaServer;
-    var administrador = req.body.administradorServer;
-    
+    var fk_cargo = req.body.fk_cargoServer;
 
-    // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
-    } else if (emailC == undefined) {
-        res.status(400).send("Sua data de nascimento está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (senhaC == undefined) {
-        res.status(400).send("Sua senha está undefined!");
-    }else if (administrador == undefined) {
-        res.status(400).send("O adm está undefined!");
-     }else {
+    } else if (fk_cargo == undefined) {
+        res.status(400).send("O cargo está undefined!");
+    } else {
 
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, administrador, fk_empresa)
-            .then(
-                function (resultado) {
-                    res.json(resultado);
-                }
-            ).catch(
-                function (erro) {
-                    console.log(erro);
-                    console.log(
-                        "\nHouve um erro ao realizar o cadastro! Erro: ",
-                        erro.sqlMessage
-                    );
-                    res.status(500).json(erro.sqlMessage);
-                }
-            );
+    usuarioModel.cadastrar(nome, email, senha, fk_cargo, fk_empresa)
+        .then(
+            function (resultado) {
+                res.json(resultado);
+            }
+        ).catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "\nHouve um erro ao realizar o cadastro! Erro: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
     }
 }
 
 function renovar(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-
     var senha = req.body.senhaServer;
     var novaSenha = req.body.novaSenhaServer;
     var IdUsuario = req.body.IdUsuario;
-
-    // Faça as validações dos valores
+    
     if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    }else {
-
-        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+    } else {
         usuarioModel.renovar(novaSenha, senha, IdUsuario)
             .then(
                 function (resultado) {
@@ -115,50 +100,50 @@ function renovar(req, res) {
     }
 }
 
-function listar(req, res ){
+function listar(req, res) {
     var IdEmpresa = req.params.IdEmpresa;
 
-    usuarioModel.listar(IdEmpresa).then(function(resultado){
-        if (resultado.length > 0){
+    usuarioModel.listar(IdEmpresa).then(function (resultado) {
+        if (resultado.length > 0) {
             res.status(200).json(resultado);
-        }else {
+        } else {
             res.status(204).send("Nenhum resultado encontrado")
         }
-    }).catch(function(erro){
+    }).catch(function (erro) {
         console.log(erro);
         console.log("Houve um erro ao buscar os usuários: ", erro.sqlMessage);
         res.status(500).json(erro.sqlMessage);
-        
+
     })
 };
 
-    function deletar (req, res){
-        var IdUsuario = req.params.idUsuario;
+function deletar(req, res) {
+    var IdUsuario = req.params.idUsuario;
 
-        usuarioModel.deletar(IdUsuario).then(function(resultado){
-            res.json(resultado);
-        }).catch(function (erro){
-           console.log(erro);
-           console.log("Houve um erroo ao buscar os usários: ", erro.sqlMessage);
-           res.status(500).json(erro.sqlMessage);
+    usuarioModel.deletar(IdUsuario).then(function (resultado) {
+        res.json(resultado);
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erroo ao buscar os usários: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
     });
-    }
+}
 
-    function redefinirSenha (req, res) {
-        
+function redefinirSenha(req, res) {
+
     console.log(req.params.idUsuario)
 
-        var id_usuario = req.params.idUsuario;
-        const senhaAntiga = req.body.senhaAntiga;
-        const novaSenha = req.body.novaSenha;
+    var id_usuario = req.params.idUsuario;
+    const senhaAntiga = req.body.senhaAntiga;
+    const novaSenha = req.body.novaSenha;
 
-        if (!id_usuario || !senhaAntiga || !novaSenha) {
-            return res.status(400).send("Dados insuficientes.");
-        }
+    if (!id_usuario || !senhaAntiga || !novaSenha) {
+        return res.status(400).send("Dados insuficientes.");
+    }
 
-        usuarioModel.verificarSenha(id_usuario, senhaAntiga)
+    usuarioModel.verificarSenha(id_usuario, senhaAntiga)
         .then(resultado => {
-            if(resultado.length === 0) {
+            if (resultado.length === 0) {
                 return res.status(401).send("Senha antiga incorreta.")
             }
             return usuarioModel.renovar(id_usuario, novaSenha)
@@ -168,8 +153,8 @@ function listar(req, res ){
             console.log(erro);
             res.status(500).send("Erro ao redefinir senha.");
         })
-    }
-    
+}
+
 
 function verificar(req, res) {
 
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -17,10 +17,10 @@ function autenticar(email, senha) {
     return database.executar(instrucaoSql);
 }
 
-function cadastrar(nome, email, senha, administrador, fk_empresa) {
+function cadastrar(nome, email, senha, fk_cargo, fk_empresa) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, senha, email, administrador, fk_empresa);
     var instrucaoSql = `
-        INSERT INTO usuarios (nome, email, senha, administrador, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${administrador}', '${fk_empresa}');
+        INSERT INTO usuarios (nome, email, senha, administrador, fk_empresa) VALUES ('${nome}', '${email}', '${senha}', '${fk_cargo}', '${fk_empresa}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
