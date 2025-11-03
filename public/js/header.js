0b383fd4ec2dfb52cc1dd26c46fdf9a60207e309
Gabi476 <gabriele.dutra@sptech.school>
2025-11-03 05:23:30 -0300

    Atualização do header

--- public/js/header.js
+++ public/js/header.js
@@ -19,7 +19,42 @@ if (header) {
             </div>
         </div>
     </nav>`
-} else if (sidebar) {
+} else if (sidebar && sessionStorage.getItem('CARGO') == "RH") {
+    sidebar.innerHTML = ` 
+        <aside class="sidebar">
+        <nav class="navbar-color d-flex flex-column flex-shrink-0">
+        <div class="container-fluid d-flex w-100 align-items-center p-1 h-100">
+            <ul class="nav nav-pills nav-flush flex-column mb-auto text-center h-100" id="ul_links">
+                    <a class="d-flex p-3 link-dark text-decorartion-none" href="index.html">
+                        <img class="bi" height="50" style="margin-left: -10px" src="assets/icons/logo-paymetrics-sem-fundo.png"/>
+                    </a>
+                <li>
+                    <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="deletarUsuario.html">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><rect width="24" height="24" fill="none"/><path fill="#fff" d="M5 19h1.425L16.2 9.225L14.775 7.8L5 17.575zm-2 2v-4.25L16.2 3.575q.3-.275.663-.425t.762-.15t.775.15t.65.45L20.425 5q.3.275.438.65T21 6.4q0 .4-.137.763t-.438.662L7.25 21zM19 6.4L17.6 5zm-3.525 2.125l-.7-.725L16.2 9.225z"/></svg>
+                        <span class="user">Editar usuários</span>
+                    </a>
+                </li>
+                    
+                <li>
+                    <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./perfil.html">
+                        <img role="img" class="bi mask" width="32" height="32" src="assets/icons/account-icon.png" alt="foto_perfil">
+                        <span class="user">Usuário</span>
+                    </a>
+                </li>
+                
+                <li class="mt-auto">
+                    <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./index.html" onclick="limparSessao()">
+                        <img role="img" class="bi mask" width="32" height="32" src="assets/icons/iconesair.png" alt="foto_perfil">
+                        <span class="user">Sair</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  </aside>
+        `;
+
+} else if (sidebar && sessionStorage.getItem('CARGO') == "Técnico") {
     sidebar.innerHTML = ` 
         <aside class="sidebar">
         <nav class="navbar-color d-flex flex-column flex-shrink-0">
@@ -29,28 +64,16 @@ if (header) {
                         <img class="bi" height="50" style="margin-left: -10px" src="assets/icons/logo-paymetrics-sem-fundo.png"/>
                     </a>
                 <li class="nav-item">
-                    <a class="nav-link py-3 d-flex mx-1 align-items-center" title data-bs-toggle="tooltip" data-bs-placemnet="right" arial-current="page" data-bs-original-title="Avisos" href="./embreve.html">
+                    <a class="nav-link py-3 d-flex mx-1 align-items-center" title data-bs-toggle="tooltip" data-bs-placemnet="right" arial-current="page" data-bs-original-title="Avisos" href="./dashAlerta.html">
                         <img src="assets/icons/alert-icon.png" class="bi mask1" aria-label="Avisos" width="32" height="32" alt="">
                         <span class="alertas">Alertas</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                      <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./embreve.html">
-                        <img src="assets/icons/chart-icon.png" class="bi mask2" width="32" height="32" alt="">
-                        <span class="dash">Dashboards</span>
-                      </a>                    
-                </li>
-                 <li class="nav-item">
                       <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./servidor.html ">
                         <img src="assets/icons/servidor.png" class="bi mask2" width="32" height="32" alt="">
                         <span class="dash">Servidores</span>
                       </a>                    
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right"href="./dashAlertas.html">
-                        <img src="assets/imgs/image_alerta.png" class="bi mask3" width="32" height="32" alt="">
-                        <span class="config">Dashboard Alertas</span>
-                    </a>
                 </li>
                 <li>
                     <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./perfil.html">
@@ -58,6 +81,48 @@ if (header) {
                         <span class="user">Usuário</span>
                     </a>
                 </li>
+                
+                <li class="mt-auto">
+                    <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./index.html" onclick="limparSessao()">
+                        <img role="img" class="bi mask" width="32" height="32" src="assets/icons/iconesair.png" alt="foto_perfil">
+                        <span class="user">Sair</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  </aside>
+        `;
+
+} else if (sidebar && sessionStorage.getItem('CARGO') == "Analista") {
+    sidebar.innerHTML = ` 
+        <aside class="sidebar">
+        <nav class="navbar-color d-flex flex-column flex-shrink-0">
+        <div class="container-fluid d-flex w-100 align-items-center p-1 h-100">
+            <ul class="nav nav-pills nav-flush flex-column mb-auto text-center h-100" id="ul_links">
+                    <a class="d-flex p-3 link-dark text-decorartion-none" href="index.html">
+                        <img class="bi" height="50" style="margin-left: -10px" src="assets/icons/logo-paymetrics-sem-fundo.png"/>
+                    </a>
+                <li class="nav-item">
+                      <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./DashPessoaEstrategia.html">
+                        <img src="assets/icons/chart-icon.png" class="bi mask2" width="32" height="32" alt="">
+                        <span class="dash">Dashboard</span>
+                      </a>                    
+                </li>
+                
+                 <li class="nav-item">
+                      <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./servidor.html ">
+                        <img src="assets/icons/servidor.png" class="bi mask2" width="32" height="32" alt="">
+                        <span class="dash">Servidores</span>
+                      </a>                    
+                </li>
+                <li>
+                    <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./perfil.html">
+                        <img role="img" class="bi mask" width="32" height="32" src="assets/icons/account-icon.png" alt="foto_perfil">
+                        <span class="user">Usuário</span>
+                    </a>
+                </li>
+                
                 <li class="mt-auto">
                     <a class="nav-link py-3 d-flex mx-1 align-items-center" data-bs-placemnet="right" href="./index.html" onclick="limparSessao()">
                         <img role="img" class="bi mask" width="32" height="32" src="assets/icons/iconesair.png" alt="foto_perfil">
