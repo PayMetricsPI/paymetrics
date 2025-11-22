0be296a64ba6cd261086f1f1ad4289b14b0e34d9
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-11-22 16:38:59 -0300

    feat: integrando front com back das noticias

--- public/js/carrossel.js
+++ public/js/carrossel.js
@@ -1,11 +1,11 @@
 const carrossel = document.getElementById("carrossel");
-const cards = document.querySelectorAll(".card_ca");
 
 let index = 0;
-const cardWidth = cards[0].offsetWidth + 16;
 
 document.getElementById("next").onclick = () => {
-    if (index < cards.length - 3) {
+    const cards = document.querySelectorAll(".card_ca");
+    const cardWidth = cards[0].offsetWidth + 16;
+    if (index < cards.length - 2) {
         index++;
     }else{
         index = 0
@@ -14,10 +14,12 @@ document.getElementById("next").onclick = () => {
 };
 
 document.getElementById("prev").onclick = () => {
+    const cards = document.querySelectorAll(".card_ca");
+    const cardWidth = cards[0].offsetWidth + 16;
     if (index > 0) {
         index--;
     }else{
-        index = cards.length - 3
+        index = cards.length - 2
     }
     carrossel.style.transform = `translateX(${-cardWidth * index}px)`;
 };
--- public/js/noticias_bruno.js
+++ public/js/noticias_bruno.js
@@ -1,7 +1,56 @@
-function obterNoticias(){
-    fetch("/news/obter").then(res => {
-        res.json().then(data => {
-            console.log(data)
-        })
-    })
+function validarImagem(url) {
+    return new Promise(resolve => {
+        const img = new Image();
+        img.src = url;
+
+        img.onload = () => resolve(true);
+        img.onerror = () => resolve(false);
+    });
+}
+
+async function obterNoticias() {
+    const res = await fetch("/news/obter");
+    const data = await res.json();
+
+    const carrossel = document.getElementById("carrossel");
+
+    for (const news of data) {
+        let imgUrl = news.Image;
+
+        if (imgUrl && imgUrl.trim() !== "") {
+            const valida = await validarImagem(imgUrl);
+            if (!valida) {
+                imgUrl = "https://static.vecteezy.com/ti/vetor-gratis/p1/25926904-jornal-plano-icone-noticia-simbolo-logotipo-ilustracao-vetor.jpg";
+            }
+        } else {
+            imgUrl = "https://static.vecteezy.com/ti/vetor-gratis/p1/25926904-jornal-plano-icone-noticia-simbolo-logotipo-ilustracao-vetor.jpg";
+        }
+
+        console.log(news)
+
+        carrossel.innerHTML += `
+            <div class="card_ca">
+                <div class="top">
+                    <div class="image_text">
+                        <img src="${imgUrl}">
+                        <h4>${news.Title}</h4>
+                    </div>
+                </div>
+                <div class="ia_explicacao">
+                    <img src="assets/imgs/gemini-color.png">
+                    <div class="explicacao">
+                        ${news.explicacao}
+                    </div>
+                </div>
+                <div class="publicada_em">
+                    Publicada em: ${new Date(news.PublishedAT.replaceAll("\"", "")).toLocaleDateString("pt-BR", {
+                        timeZone: "UTC",
+                        month: "numeric",
+                        year: "numeric",
+                        day: "2-digit"
+                    })}
+                </div>
+            </div>
+        `;
+    }
 }
