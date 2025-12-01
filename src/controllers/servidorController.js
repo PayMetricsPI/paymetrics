55deba8bcb0a150aca3c5a4faf77ceb3958a9cb7
SamuelAntunes2006 <samuel.asantos@sptech.school>
2025-12-01 09:16:17 -0300

    feat: Adiciona suporte para IP EC2 na atualização de servidores e implementa funções de mapa global e estados

--- src/controllers/servidorController.js
+++ src/controllers/servidorController.js
@@ -53,13 +53,13 @@ function deletarServidor(req, res) {
 
 function atualizarServidor(req, res) {
     const id_servidor = req.params.id_servidor;
-    const { nome,pais, estado, mac_address, tipo_cpu, ram, disco } = req.body;
+    const { nome,pais, estado, mac_address, tipo_cpu, ram, disco,ipEc2 } = req.body;
 
-    if (!id_servidor || !nome || !mac_address || !pais || !estado || !tipo_cpu || !ram || !disco) {
+    if (!id_servidor || !nome || !mac_address || !ipEc2 || !pais || !estado || !tipo_cpu || !ram || !disco) {
         return res.status(400).json({ error: "Dados incompletos para atualizar servidor" });
     }
 
-    servidorModel.atualizarServidor(id_servidor, nome,pais, estado, mac_address, tipo_cpu, ram, disco,)
+    servidorModel.atualizarServidor(id_servidor, nome,pais, estado, mac_address,ipEc2, tipo_cpu, ram, disco,)
         .then(resultado => res.status(200).json({ message: "Servidor atualizado com sucesso", resultado }))
         .catch(erro => {
             console.error("Erro ao atualizar o servidor:", erro.sqlMessage || erro);
@@ -67,10 +67,31 @@ function atualizarServidor(req, res) {
         });
 }
 
+function mapaGlobal(req, res) {
+    const fk_empresa = req.params.fk_empresa;
+
+    servidorModel.mapaGlobal(fk_empresa)
+        .then(resultado => res.json(resultado))
+        .catch(erro => res.status(500).json(erro));
+}
+
+function mapaEstados(req, res) {
+    const fk_empresa = req.params.fk_empresa;
+    const pais = req.params.pais;
+
+    servidorModel.mapaEstados(fk_empresa, pais)
+        .then(resultado => res.json(resultado))
+        .catch(erro => res.status(500).json(erro));
+}
+
+
 
 module.exports = {
     atualizarServidor,
     listarServidores,
     criarServidores,
-    deletarServidor
+    deletarServidor,
+    mapaGlobal,
+    mapaEstados
+
 };
