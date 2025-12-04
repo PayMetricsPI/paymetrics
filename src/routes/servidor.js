6026534873b11345d3beb9101c922dc0f674df36
SamuelAntunes2006 <samuel.asantos@sptech.school>
2025-12-04 15:00:33 -0300

    feat: Refatora rotas do servidor para melhorar a organização e legibilidade

--- src/routes/servidor.js
+++ src/routes/servidor.js
@@ -3,16 +3,26 @@ var router = express.Router();
 
 var servidorController = require("../controllers/servidorController");
 
-// ROTAS MAIS ESPECÍFICAS PRIMEIRO
+
+router.get("/:fk_empresa", function(req, res) {
+    servidorController.listarServidores(req, res);
+});
+
+
+router.post("/criarServidor", function(req, res) {
+    servidorController.criarServidores(req, res);
+});
+
+router.put("/atualizarServidor/:id_servidor", function(req, res) {
+    servidorController.atualizarServidor(req, res);
+});
+
+
+router.post("/deletarServidor/:id_servidor", function(req, res) {
+    servidorController.deletarServidor(req, res);
+});
+
 router.get("/mapa/:fk_empresa/:pais", servidorController.mapaEstados);
 router.get("/mapa/:fk_empresa", servidorController.mapaGlobal);
-
-// SÓ DEPOIS A GENÉRICA
-router.get("/:fk_empresa", servidorController.listarServidores);
-
-// CRUD
-router.post("/criarServidor", servidorController.criarServidores);
-router.put("/atualizarServidor/:id_servidor", servidorController.atualizarServidor);
-router.post("/deletarServidor/:id_servidor", servidorController.deletarServidor);
 
 module.exports = router;
